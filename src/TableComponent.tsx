ba3771a9be661457b1141f6c41d92d6ff7e31bce
Yerbol Syzdyk <yerbol_syzdyk@epam.com>
2023-11-21 15:54:20 +0600

    EPMDCEMLST-3807: change HeaderSearch search method
    Change-Id: I84deb4b60e7e323813095ead39b6d324876c80b5

--- src/TableComponent.tsx
+++ src/TableComponent.tsx
@@ -286,7 +286,7 @@ export type TableInstance<TData extends Record<string, any> = {}> = Omit<
 	getPresets: () => Preset[]
 	savePresets: (presets: Preset[]) => void
 	getDefaultPresets: () => Preset[]
-	showSearchData: (searchId: string | string[] | null) => void
+	setSearchData: (data: SearchData<TData>) => void
 	setHighlightHeadCellId: (colId: string | null) => void
 	options: TableComponentProps<TData> & {
 		icons: Table_Icons
@@ -337,6 +337,8 @@ export type TableInstance<TData extends Record<string, any> = {}> = Omit<
 
 export type FieldError = string | null
 
+export type SearchData<TData extends TableData = TableData> = TData[] | null
+
 export type Table_TableState<TData extends Record<string, any> = {}> =
 	TableState & {
 		columnFilterFns: Record<string, Table_FilterOption>
@@ -353,13 +355,13 @@ export type Table_TableState<TData extends Record<string, any> = {}> =
 		hoveredRow: HoveredRowState<TData>
 		isFullScreen: boolean
 		isLoading: boolean
+		searchData: SearchData<TData>
 		showAlertBanner: boolean
 		showColumnFilters: boolean
 		showGlobalFilter: boolean
 		showProgressBars: boolean
 		showSkeletons: boolean
 		showToolbarDropZone: boolean
-		searchId: string | null
 		highlightHeadCellId: string | null
 		groupCollapsed: GroupCollapsed
 		stickyHorizontalScrollbarHeight: number
@@ -1483,7 +1485,6 @@ export type TableComponentProps<TData extends Record<string, any> = {}> = Omit<
 	onGetPresets?: () => Preset[]
 	onSavePresets?: (presets: Preset[]) => void
 	onGetDefaultPresets?: (state?: PresetState) => Preset[]
-	onSearchData?: () => void
 	onNativeEvent?: ({
 		el,
 		type,
